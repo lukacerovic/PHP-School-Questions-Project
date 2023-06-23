6b60b87e4abf4ba05e94d4968de815e90d2ee65d
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2023-06-23 15:33:22 +0200

    connect.php

--- SchoolManagementSystem/classes/connect.php
+++ SchoolManagementSystem/classes/connect.php
@@ -7,7 +7,7 @@ class Database
     private $host = "localhost";
     private $username = "root";
     private $password = "";
-    private $db = "stefanov_projekat_db";
+    private $db = "school_db";
 
     function connect()
     {
@@ -18,7 +18,7 @@ class Database
 
     function read($query)
     {
-        $conn = $this->connect(); //ukljucujes connect funkciju u read funkciju
+        $conn = $this->connect(); //including connect function in read function
         $result = mysqli_query($conn, $query);
 
         if(!$result)
@@ -30,7 +30,7 @@ class Database
             $data = false; 
             while($row = mysqli_fetch_assoc($result))
             {
-                $data[] = $row; // ovde sam stavio ako bude bilo vise rezultata da se onda belezi u varijabli data koja ce biti 'array';
+                $data[] = $row;
             }
             return $data;
         }
@@ -52,29 +52,3 @@ class Database
         
     }
 }
-
-
-
-//Ne znam da li treba da se admin napravi manualno u bazi podataka ili treba da se namesti inicijalno kroz kod
-//ili da se in on moze napraviti klasicnom registracijom
-
-
-//Postavljanje admina:
-//$first_name = "stefan";
-//$last_name = "suvajcevic";
-//$user_name = "stefan-admin1";
-//$email = "stefanadmin@gmail.com";
-//$password = "admin12345";
-//$phone = "+381 60 6016420";
-//$date_of_birth = "2000-04-16";
-//$usertype = "admin";
-
-//proveri da li si u kreiranju baze u phpmyadminu stavio za polje id da bude autoincrement (AI checkbox da selektujes)
-
-//formiranje upita za ubacivanje admina u bazu:
-//vrednosti varijabli stavljas da budu pod navodnicima (stavljas jedne navodnike a ne duple jer duple vec koristis za sql upit) jer su vrednostni reci a ne brojevi)
-//$admin = "INSERT INTO users (first_name, last_name, username, email, password, phone, date_of_birth, usertype) 
-          //VALUES ('$first_name', '$last_name', '$username', '$email', '$password', '$phone', '$date_of_birth', '$usertype') limit 1";
-
-//izvrsavanje funkcije:
-//mysqli_query($connection, $admin);
