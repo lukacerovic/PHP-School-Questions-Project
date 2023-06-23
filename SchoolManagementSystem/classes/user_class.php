7e87448782f5f45f3541d520f3e4040f93cbaae2
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2023-06-23 15:48:55 +0200

    user_class.php

--- SchoolManagementSystem/classes/user_class.php
+++ SchoolManagementSystem/classes/user_class.php
@@ -1,9 +1,8 @@
 <?php
 
-//prikupljanje informacija o korisniku
 class User
 {
-    //zaduzena za izvlacenje tacno ulogovanog korisnika
+    //finding logged in user data
     public function get_data($id)
     {
 
@@ -23,11 +22,11 @@ class User
             return false;
         }
     }
-    //izvlacimo sve korisnike iz tabele users
+    //Getting all users from user table
     public function get_all_users()
     {
-        // Dobijanje liste korisnika iz baze podataka
-        $users = array(); // Prazan niz za čuvanje korisnika
+        //Adding users in a list
+        $users = array(); // empty array for collecting users
         $query = "SELECT * FROM users";
         $DB = new Database();
         $result = $DB->read($query);
@@ -40,11 +39,11 @@ class User
         }
 
     }
-    //ovde izvlacimo sve korisnike koji su tipa 'user' ili 'User'
+    //Getting all profiles with usertype == 'User'
     public function get_type_user()
     {
         $users = array(); // Prazan niz za čuvanje korisnika
-        $query = "SELECT * FROM users WHERE usertype = 'user' or usertype = 'User'";
+        $query = "SELECT * FROM users WHERE usertype = 'User'";
         $DB = new Database();
         $result = $DB->read($query);
         if ($result) {
